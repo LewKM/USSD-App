17f4687265916ef7e5e23a27c294eab19eec2295
LewKM <lewiskathembe51@gmail.com>
2024-05-24 14:21:08 +0300

    Adds the callabck function

--- callback.php
+++ callback.php
@@ -4,3 +4,11 @@ header("Access-Control-Allow-Origin: *");
 $stkCallbackResponse = file_get_contents('php://input');
 
 $logFile = "stkCallbackResponse.json";
+
+$data = '{}';
+
+file_put_contents($logFile, $data);
+
+$log = fopen($logFile, 'a');
+fwrite($log, $stkCallbackResponse);
+fclose($log);
